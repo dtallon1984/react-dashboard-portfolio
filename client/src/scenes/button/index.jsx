f41a32a5721b07664cab7843637e7984bdd3d89b
David Tallon <dtallon1984@gmail.com>
2025-03-07 16:35:03 -0600

    style updates to button and accordion components

--- client/src/scenes/button/index.jsx
+++ client/src/scenes/button/index.jsx
@@ -5,8 +5,28 @@ function ButtonDemo() {
   const handleClick = () => {
     console.log("click!");
   };
+  const containerStyle = {
+    position: 'relative', // Needed for absolute positioning of child
+    height: '25vh',
+    width: '25vw'
+  };
+  
+  const contentStyle = {
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)', // Centers the element
+     width: '200px',
+    height: '100px',
+    //backgroundColor: '#f0f0f0',
+    //border: '1px solid #ccc',
+    //textAlign: 'center'
+  };
   return (
-    <div>
+
+    <div style={containerStyle} > 
+    <div style={contentStyle} >
+      <h1>Buttons</h1>
       <div>
         <Button onClick={handleClick}>Plain</Button>
       </div>
@@ -37,6 +57,7 @@ function ButtonDemo() {
         <Button danger rounded>
           Danger
         </Button>
+      </div>
       </div>
     </div>
   );
